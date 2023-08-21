48f1967b84bfcd70a0da8a489440618b7bc058c1
gshalom910 <91600692+gshalom910@users.noreply.github.com>
2023-08-21 22:38:20 +0300

    initial commit

---
+++ src/component/AnswerQusetion/AnswerQuestion.jsx
@@ -0,0 +1,51 @@
+import { useContext, useState } from "react";
+import "./answerQuestion.css";
+import { UserContext } from "../Context/UserContext";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function AnswerQuestion({ questionId }) {
+  const [userData, setUserData] = useContext(UserContext);
+  const [form, setForm] = useState({});
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${process.env.REACT_APP_base_url}/api/answer`, {
+        id: userData.user.id,
+        questionId: questionId,
+        answer: form.answer,
+      });
+      window.location.reload(false);
+    } catch (err) {
+      console.log("problem", err);
+    }
+  };
+  return (
+    <div className="container my-5">
+      <form
+        onSubmit={handleSubmit}
+        className="d-flex flex-column p-5 answer_form  justify-content-between"
+      >
+        <h3 className="">Answer The Top Question</h3>
+        <Link to="/" className="text-decoration-none text-reset cursor-pointer">
+          Go to Question page
+        </Link>
+        <textarea
+          onChange={handleChange}
+          className="answer_input"
+          placeholder="Your Answer..."
+          name="answer"
+          id=""
+        ></textarea>
+        <button className="answer_post_btn" type="">
+          Post Your Answer
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AnswerQuestion;
