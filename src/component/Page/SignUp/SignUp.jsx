540a1bb10cc0090f06e62646e0cbd9686769c55d
gshalom910 <91600692+gshalom910@users.noreply.github.com>
2023-08-24 00:15:41 +0300

    update sign up

--- src/component/Page/SignUp/SignUp.jsx
+++ src/component/Page/SignUp/SignUp.jsx
@@ -18,9 +18,9 @@ function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`${process.env.REACT_APP_base_url}api/users`, form);
+      await axios.post(`${process.env.REACT_APP_base_url}/api/users`, form);
       const loginRes = await axios.post(
-        `${process.env.REACT_APP_base_url}api/users/login`,
+        `${process.env.REACT_APP_base_url}/api/users/login`,
         {
           email: form.email,
           password: form.password,
