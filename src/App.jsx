2764a1c4e7e15773d9137f2c306f2c84df1be08c
gshalom910 <91600692+gshalom910@users.noreply.github.com>
2023-08-29 12:30:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           headers: { "x-auth-token": token },
         }
       );
-      // console.log(userRes);
+      console.log(userRes);
       setUserData({
         token,
         user: {
